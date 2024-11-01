70a92a551690b547176bb3d44482391837912494
ozaki852 <ozaki852@gmail.com>
2024-11-02 01:43:30 +0800

    初始化 Spring Boot Project

---
+++ springboot-mall/src/test/java/com/mall/springbootmall/SpringbootMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.springbootmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
